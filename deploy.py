6aa472536de19b6adcd885f07533335bd9591998
TanvirSinghAneja <tsaneja@gmail.com>
2026-05-29 15:39:03 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,10 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import io
-import warnings
-warnings.filterwarnings('ignore')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
